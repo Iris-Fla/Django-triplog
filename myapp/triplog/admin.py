4fe2f061685b4c17d7681f3a20b4de495b1862b5
Iris-Fla <irith.fla@gmail.com>
2024-10-08 12:39:25 +0900

    loginの実装

--- myapp/triplog/admin.py
+++ myapp/triplog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from triplog.models import Triplog
 
+admin.site.register(Triplog)
 # Register your models here.
--- myapp/triplog/views.py
+++ myapp/triplog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from triplog.models import Triplog
 
 def index(request):
+    all_data = Triplog.objects.all()
     context = {
         'title':'旅行の記録 - 一覧画面',
+        'all_data':all_data,
     }
     return render(request, 'triplog/index.html',context)
